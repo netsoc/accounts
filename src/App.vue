8cff46ce6bc4b6d47ca5390697f0cd9571e43b8d
darts <darts@tcd.ie>
2020-09-18 18:19:35 +0100

    A different vue of it

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <img id="netsoc_logo" alt="Netsoc logo" src="./assets/netsoc-logo.svg">
+    <LoginPrompt />
+  </div>
+</template>
+
+<script>
+import LoginPrompt from './components/Login.vue'
+
+export default {
+  name: 'App',
+  components: {
+    LoginPrompt
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+#netsoc_logo {
+  max-height: 5rem;
+}
+</style>
---
+++ src/components/Login.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="login_container">
+    <div class="">Username:</div>
+    <input v-model="username" type="text" />
+    <span v-if="no_username">Please enter your username</span>
+    <div>Password:</div>
+    <input v-model="password" type="password" />
+    <span v-if="no_password">Please enter a password</span>
+    <button v-on:click="login">Login</button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "LoginPrompt",
+  data() {
+    return {
+      username: "",
+      password: "",
+      no_username: false,
+      no_password: false,
+    };
+  },
+  methods: {
+    login() {
+      let username = this.username;
+      let password = this.password;
+      this.no_username = username.length < 1;
+      this.no_password = password.length < 1;
+
+      if (this.no_username || this.no_password) return;
+
+      alert({ username: username, password: password });
+    },
+  },
+};
+</script>
+
+<style scoped>
+#login_container {
+    border-radius: 1rem;
+}
+
+</style>
