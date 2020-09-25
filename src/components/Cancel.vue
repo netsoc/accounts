ba982985b536478cc06f2336790a880ddc69722c
darts <darts@tcd.ie>
2020-09-25 02:55:42 +0100

    Success and cancel pages

---
+++ src/components/Cancel.vue
@@ -0,0 +1,24 @@
+<template>
+  <p>
+    Transaction cancelled.
+    Your account will not be updated.
+    Redirecting to login page...
+  </p>
+</template>
+
+<script>
+export default {
+  name: "Cancel",
+  data() {
+    return {};
+  },
+  methods: {
+    redirectToLogin() {
+      setTimeout(() => this.$router.push({ name: "Login" }), 2500);
+    },
+  },
+  beforeMount() {
+    this.redirectToLogin();
+  },
+};
+</script>
---
+++ src/components/Success.vue
@@ -0,0 +1,24 @@
+<template>
+  <p>
+    Transaction Succeeded!
+    Your account has been updated.
+    Redirecting to login page...
+  </p>
+</template>
+
+<script>
+export default {
+  name: "Success",
+  data() {
+    return {};
+  },
+  methods: {
+    redirectToLogin() {
+      setTimeout(() => this.$router.push({ name: "Login" }), 2500);
+    },
+  },
+  beforeMount() {
+    this.redirectToLogin();
+  },
+};
+</script>
