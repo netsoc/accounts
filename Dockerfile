2725d1ed829e11f369fab1fe71df8ae9f6361eb5
Jack O'Sullivan <jackos1998@gmail.com>
2020-09-26 15:50:32 +0100

    Make production frontend image configurable through environment

--- Dockerfile
+++ Dockerfile
@@ -14,3 +14,19 @@ RUN rm /etc/nginx/conf.d/*
 COPY nginx.conf /etc/nginx/conf.d/site.conf
 
 COPY --from=builder /opt/accounts/dist/ /srv/accounts/
+
+ENV IAM_BASE_URL=http://localhost:8080/v1 \
+    IAM_BASE_URL=/users/\${username}/login \
+    IAM_VERIFY_URL=/users/self/login \
+    IAM_CREATE_ACCOUNT_URL=/users \
+    IAM_GET_USER_DATA_URL=/users/self \
+    IAM_UPDATE_USER_DATA_URL=/users/self
+
+ENV PAYMENT_BASE_URL=http://localhost:4242 \
+    PAYMENT_CREATE_SESSION_URL=/create/session
+
+ENV STRIPE_PUBLIC_KEY=
+
+COPY entrypoint.sh /accounts-entrypoint.sh
+ENTRYPOINT ["/accounts-entrypoint.sh"]
+CMD ["nginx", "-g", "daemon off;"]
---
+++ entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+set -e
+
+cat <<EOF > /srv/accounts/config.js
+// IAM
+const IAM_BASE_URL = "$IAM_BASE_URL";
+const IAM_LOGIN_URL = IAM_BASE_URL + "${IAM_LOGIN_URL}"
+const IAM_VERIFY_URL = IAM_BASE_URL + "${IAM_VERIFY_URL}"
+const IAM_CREATE_ACCOUNT_URL = IAM_BASE_URL + "${IAM_CREATE_ACCOUNT_URL}"
+const IAM_GET_USER_DATA_URL = IAM_BASE_URL + "${IAM_GET_USER_DATA_URL}"
+const IAM_UPDATE_USER_DATA_URL = IAM_BASE_URL + "${IAM_UPDATE_USER_DATA_URL}"
+
+// Payments Service
+const PAYMENT_BASE_URL = "${PAYMENT_BASE_URL}"
+const PAYMENT_CREATE_SESSION_URL = PAYMENT_BASE_URL + "${PAYMENT_CREATE_SESSION_URL}"
+
+// Stripe
+const STRIPE_PUBLIC_KEY = '${STRIPE_PUBLIC_KEY}';
+EOF
+
+exec /docker-entrypoint.sh "$@"
